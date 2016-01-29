472c0e7bba4d84fdef4e841f8b5e964841f6a229
pkertesz <peter@kertesz-it.de>
2016-01-29 12:14:51 +0100

    added a comment

--- src/main/java/com/naturalprogrammer/spring/demo/DemoApplication.java
+++ src/main/java/com/naturalprogrammer/spring/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.naturalprogrammer.spring.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A comment
+ * @author Peter
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
